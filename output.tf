d906def577b4e3faf15f84e775379bbc3fb61126
jay9044 <jamalwestfield@gmail.com>
2024-01-03 15:24:02 +0000

    setting up module structure

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "aws_ami" {
-  value = data.aws_ami.latest-amazon-linux-image
+  value = ""
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.myapp_webserver.public_dns
+  value = ""
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.myapp_webserver.public_ip
+  value = ""
 }
