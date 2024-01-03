cf5f11bb61e3814189df5269da13d86f56a6fd5b
jay9044 <jamalwestfield@gmail.com>
2024-01-03 13:50:11 +0000

    user script typos & added readme

--- entry_point.sh
+++ entry_point.sh
@@ -1,7 +1,7 @@
-#! bin/bash
+#!/bin/bash
 
-sudo yum update -y && yum yum install -y docker
-sudo systemctrl start docker
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 docker run -p 8080:80 nginx
