7bb079f516f5e20a71fad9aa5ad897c1a3b1dde9
jay9044 <jamalwestfield@gmail.com>
2024-01-03 13:30:31 +0000

    forgot to save  user data script changes

--- entry_point.sh
+++ entry_point.sh
@@ -1,5 +1,7 @@
 #! bin/bash
 
-sudo yum update -y && sum yum install -y docker
+sudo yum update -y && yum yum install -y docker
 sudo systemctrl start docker
 sudo usermod -aG docker ec2-user
+
+docker run -p 8080:80 nginx
